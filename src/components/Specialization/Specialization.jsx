2e30b72343a90789048d4a59b62d6fdae1334f86
Khizar Ali <khizibutt123@gmail.com>
2024-04-03 02:53:19 +0500

    fixes the speciality page

--- src/components/Specialization/Specialization.jsx
+++ src/components/Specialization/Specialization.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Specialization.css";
-const Specialization = ({data,title,subtitle,backgroundColor}) => {
+const Specialization = ({ data, title, subtitle, backgroundColor }) => {
   return (
-    <div className="specialization" style={{
-      backgroundColor : {}
-    }}>
-        <h1>{title} <br /><span>{subtitle}</span></h1>
+    <div
+      className="specialization"
+      style={{
+        backgroundColor: {},
+      }}
+    >
+      <h1>
+        {title} <br />
+        <span>{subtitle}</span>
+      </h1>
       <div className="mainContainer_service specializationContainer">
         {data.map((service) =>
           service.redirect == "" ? (
@@ -14,17 +20,16 @@ const Specialization = ({data,title,subtitle,backgroundColor}) => {
               href={service.redirect}
               className="specializationContainerBox"
               key={service.id}
-              
             >
               <i>{service.icon}</i>
               <h3>{service.title}</h3>
               <span>{service.description}</span>
             </div>
           ) : (
-            <Link to = {service.redirect}
-            target="_blank"
-              key={service.id} 
-              className="specializationContainerBox" 
+            <Link
+              to={service.redirect}
+              key={service.id}
+              className="specializationContainerBox"
             >
               <i>{service.icon}</i>
               <h3>{service.title}</h3>
